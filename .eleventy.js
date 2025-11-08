339b5f307bce99d28261b1ef3161bf3102e8a57e
Nikolai Nedovodin <info@cputer.com>
2025-11-08 01:46:47 -0800

    build(eleventy): render html/md with Nunjucks; output to dist

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,15 @@
-﻿module.exports = function (eleventyConfig) {
-  // Add any custom config you had before here
-
+﻿module.exports = function(eleventyConfig) {
   return {
-    pathPrefix: "/mind-spec/",
-    dir: { input: "src", output: "dist" }
+    dir: {
+      input: "src",
+      output: "dist",
+      includes: "_includes",
+      data: "_data"
+    },
+    templateFormats: ["njk","md","html","css","json","ico","svg","png","jpg","gif","webp"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    pathPrefix: "/mind-spec/"
   };
 };
